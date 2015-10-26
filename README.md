f83ef0b20c115e7e54865a9412e28355e4c57b46
Richard Masci <rmasci@gmail.com>
2015-10-25 21:41:46 -0400

    Updated to use IPv6

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
-sslcheck
-========
+<h1>sslcheck</h1>
+<hr>
 
-sslcheck allows you to check ssl keys with or without DNS resolution. The company I work for requires that all servers
+Sslcheck allows you to check ssl keys with or without DNS resolution. The company I work for requires that all servers
 have different ssl keys.  So if you've got two, four or more servers that sit behind a load balancer, it makes it hard
 to inspect the keys on each server individually as the load balancer is where the DNS name for that SSL key resides.
 
@@ -9,7 +9,8 @@ Sslcheck takes two parameters -i <ip address> -p <port> -d <dns name>.  The -i i
 perform any dns lookups, while the -d is manditory. The -p is for servers that might be running on a port other than 443.
 
 Sslcheck is written in Go and should compile without the need of additional packages.
-Example:
+<h1>Example:</h1>
+<h2>IPv4</h2>
 <pre>
   ] $ ./sslcheck -d www.google.com
   Client connected to: 74.125.137.99:443
@@ -29,4 +30,30 @@ Example:
   Issued by:
 	  Thawte SGC CA
 	  []
-	  [Thawte Consulting (Pty) Ltd.]</pre>
+	  [Thawte Consulting (Pty) Ltd.]</pre>Client connected to: [2607:f8b0:4002:801::1001]:443
+</pre>
+<h2>IPv6<h2>
+<pre>
+  ] $ ./sslcheck -d www.google.com
+Client connected to: [2607:f8b0:4002:801::1001]:443
+Cert Checks OK
+Server key information:
+	CN:	 *.google.com
+	OU:	 
+	Org:	 Google Inc
+	City:	 Mountain View
+	State:	 California
+	Country: US
+SSL Certificate Valid:
+	From:	 2015-10-15 16:42:43 +0000 UTC
+	To:	 2016-01-13 00:00:00 +0000 UTC
+	OK: 	Cert Expires in 78 days
+Valid Certificate DNS:
+	*.google.com
+	*.android.com
+	...
+	youtubeeducation.com
+Issued by:
+	Google Internet Authority G2
+	Google Inc
+<pre>
--- sslcheck.go
+++ sslcheck.go
@@ -4,13 +4,15 @@ import (
 	"crypto/tls"
 	"flag"
 	"fmt"
+	"net"
 	"os"
 	"path/filepath"
-	"net"
+	"strings"
 	"time"
 )
 
 var runAs = filepath.Base(os.Args[0])
+var verb bool
 
 // Usage is what is run if the right parameters are not met upon startup.
 func Usage() {
@@ -19,11 +21,16 @@ func Usage() {
 	flag.PrintDefaults()
 }
 
+const shortForm = "2006-01-02 15:04:05"
+
 func main() {
+	osExit := 0
+	timeNowSec := time.Now().UTC().Unix()
 	var ipAddress, domainName, port string
 	// define flags passed at runtime, and assign them to the variables defined above
 	flag.StringVar(&ipAddress, "i", "", "IP Address")
 	flag.StringVar(&domainName, "d", "", "Domain Name")
+	flag.BoolVar(&verb, "v",false, "Verbose")
 	flag.StringVar(&port, "p", "443", "Port Number")
 	flag.Parse()
 	if domainName == "" {
@@ -31,21 +38,34 @@ func main() {
 		os.Exit(1)
 	}
 	if ipAddress == "" {
+		if verb {
+			fmt.Println("looking up: ", domainName)
+		}
 		ip, err := net.LookupHost(domainName)
 		if err != nil {
-			fmt.Printf("Could not resolve domain name, %v.\n\n",domainName)
+			fmt.Printf("Could not resolve domain name, %v.\n\n", domainName)
 			fmt.Printf("Either supply a valid domain name or use the -i switch to supply the ip address.\n")
 			fmt.Printf("Domain name lookups are not performed when the user provides the ip address.\n")
+			Usage()
 			os.Exit(1)
 		}
-		ipAddress = ip[0] + ":" + port
+		if verb {
+			fmt.Printf("IP Addresses: %v\n", ip)
+			fmt.Println("IP Address:",ip[0])
+		}
+		ipAddress = ip[0] // + ":" + port
+	}
+	//
+	//Connect network
+	if len(ipAddress) > 16 {
+		ipAddress = "[" + ipAddress + "]:" + port
 	} else {
 		ipAddress = ipAddress + ":" + port
 	}
-	//Connect network
-	ipConn,err:=net.DialTimeout("tcp",ipAddress,60000*time.Millisecond)
+	if verb {fmt.Println("IPAddress Length",len(ipAddress))}
+	ipConn, err := net.DialTimeout("tcp", ipAddress, 60000*time.Millisecond)
 	if err != nil {
-		fmt.Printf("Could not connect to %v - %v\n",ipAddress,domainName)
+		fmt.Printf("Could not connect to %v - %v\n", ipAddress, domainName)
 		os.Exit(1)
 	} else {
 		defer ipConn.Close()
@@ -53,26 +73,48 @@ func main() {
 	// Configure tls to look at domainName
 	config := tls.Config{ServerName: domainName}
 	// Connect to tls
-	conn:= tls.Client(ipConn, &config)
+	conn := tls.Client(ipConn, &config)
 	defer conn.Close()
 	// Handshake with TLS to get cert
 	hsErr := conn.Handshake()
 	if hsErr != nil {
-		fmt.Printf("Client connected to: %v\n", conn.RemoteAddr())
 		fmt.Printf("Cert Failed for %v - %v\n", ipAddress, domainName)
 		os.Exit(1)
-	} else { 
-		fmt.Printf("Client connected to: %v\n", conn.RemoteAddr())
-		fmt.Printf("Cert Checks OK\n")
 	}
+	fmt.Printf("Client connected to: %v\n", conn.RemoteAddr())
 	state := conn.ConnectionState()
+	fmt.Printf("Cert Checks OK\n")
 	i := 0
 	for _, v := range state.PeerCertificates {
 		if i == 0 {
+			sslFrom := fmt.Sprint(v.NotBefore)
+			sslTo := fmt.Sprint(v.NotAfter)
 			fmt.Println("Server key information:")
-			fmt.Printf("\tCN:\t %v\n\tOU:\t %v\n\tOrg:\t %v\n", v.Subject.CommonName, v.Subject.OrganizationalUnit, v.Subject.Organization)
-			fmt.Printf("\tCity:\t %v\n\tState:\t %v\n\tCountry: %v\n", v.Subject.Locality, v.Subject.Province, v.Subject.Country)
-			fmt.Printf("SSL Certificate Valid:\n\tFrom:\t %v\n\tTo:\t %v\n", v.NotBefore, v.NotAfter)
+			fmt.Printf("\tCN:\t %v\n\tOU:\t %v\n\tOrg:\t %v\n", v.Subject.CommonName, strings.Trim(fmt.Sprintf("%v",v.Subject.OrganizationalUnit),"[]"), strings.Trim(fmt.Sprintf("%v",v.Subject.Organization),"[]"))
+			fmt.Printf("\tCity:\t %v\n\tState:\t %v\n\tCountry: %v\n", v.Subject.Locality[0], v.Subject.Province[0], v.Subject.Country[0])
+			fmt.Printf("SSL Certificate Valid:\n\tFrom:\t %s\n\tTo:\t %s\n", sslFrom, sslTo)
+
+			//check date...
+			ts := strings.Split(sslTo, " ")
+			timeExp, err := time.Parse(shortForm, ts[0]+" "+ts[1])
+			if err != nil {
+				fmt.Printf("Could not parse time: %v %v", ts[0], err)
+				osExit = 1
+			}
+			timeExpSec := timeExp.Unix()
+			if timeExpSec < timeNowSec {
+				fmt.Printf("\tWARNING:\t Cert has expired.\n")
+				osExit = 1
+			}
+			time2Exp := (((timeExpSec - timeNowSec) / 60) / 60) / 24
+			if time2Exp < 31 {
+				fmt.Printf("\tWARNING:\t Cert Expires in %v days\n", time2Exp)
+				osExit = 1
+
+			} else {
+				fmt.Printf("\tOK: \tCert Expires in %v days\n", time2Exp)
+				//fmt.Printf("TimeNow: %v, TimeCert: %v\n", timeNowSec, timeExpSec)
+			}
 			fmt.Printf("Valid Certificate DNS:\n")
 			if len(v.DNSNames) >= 1 {
 				for dns := range v.DNSNames {
@@ -83,11 +125,12 @@ func main() {
 			}
 			i++
 		} else if i == 1 {
-			fmt.Printf("Issued by:\n\t%v\n\t%v\n\t%v\n", v.Subject.CommonName, v.Subject.OrganizationalUnit, v.Subject.Organization)
+			fmt.Printf("Issued by:\n\t%v\n\t%v\n\t%v\n", v.Subject.CommonName, strings.Trim(fmt.Sprintf("%v",v.Subject.OrganizationalUnit),"[]"), strings.Trim(fmt.Sprintf("%v",v.Subject.Organization),"[]"))
 			i++
 		} else {
 			break
 		}
 	}
+	os.Exit(osExit)
 
 }
